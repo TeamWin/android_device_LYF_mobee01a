609d1d82ef3bd40ae8d01334049a22870d30dd29
Mohd Faraz <Checkingprop@gmail.com>
2017-12-13 20:15:43 +0530

    LS5015: Renamed to Original

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,10 +53,16 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_PREBUILT_DT := true
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/LS5015-kernel/boot.img-dt
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/LS5015-kernel/zImage
+#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/LS5015-kernel/boot.img-dt
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/LYF/mobee01a
+TARGET_KERNEL_CONFIG := test01a_msm_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/LS5015-kernel/zImage
 
 
 #Encryption
