88c6ac85f43460a956a61f1cbddcd0c7852686a8
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-03-30 11:44:29 +0000

    LS-5015: PitchBlack bringup! & Rebranded to original name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,21 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Product-specific compile-time definitions.
-#
 
-LOCAL_PATH := device/LYF/LS5015
-#CRYPT_PATH := device/qcom/common/
-
-TARGET_BOARD_VENDOR := LYF
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-TARGET_NO_BOOTLOADER := true
+LOCAL_PATH := device/LYF/mobee01a
 
 # Architecture
-TARGET_BOARD_SUFFIX := _64
-
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -41,72 +30,69 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_VARIANT := msm8939
+TARGET_BOARD_PLATFORM := msm8939
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
+TARGET_BOARD_SUFFIX := _64
 
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/LS5015-kernel/boot.img-dt
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_APPEND_DTB := true
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/LYF/LS5015
-TARGET_KERNEL_CONFIG := nichrome_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
 
-#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/LS5015-kernel/zImage
-
-
-#Encryption
+# Encryption
 TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_CRYPTFS_HW_PATH := $(LOCAL_PATH)/cryptfs_hw
+
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Partitions
-TARGET_USERIMAGES_USE_F2FS := true
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_BOOTIMAGE_PARTITION_SIZE      := 0x002000000
-BOARD_CACHEIMAGE_PARTITION_SIZE     := 0x018000000
-BOARD_PERSISTIMAGE_PARTITION_SIZE   := 0x002000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 0x002000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 0x068000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE  := 0x307dfbc00
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11556847 #(11556847-16384)
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-#TWRP config
-TW_CUSTOM_THEME := $(LOCAL_PATH)/recovery/res
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
-#Recovery
-BOARD_SUPPRESS_EMMC_WIPE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
-#TWHAVE_SELINUX := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_EXCLUDE_SUPERSU := true
-TW_INCLUDE_CRYPTO := true
-TW_MTP_DEVICE := "/dev/mtp_usb"
-#TW_NO_USB_STORAGE := true
-TW_OEM_BUILD := false
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# TWRP Configuration
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_EXTRA_LANGUAGES := true
 TW_THEME := portrait_hdpi
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
-#Mouse c ursor
+# Disable Mouse Cursor
 TW_INPUT_BLACKLIST := "hbtp_vm"
 
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
--- README.md
+++ README.md
@@ -1,16 +1,22 @@
-Device configuration for LYF Water 8(LS5015).
-==================================================
+TeamWin Recovery Project
+
+Device configuration for Xiaomi Redmi 4[IN]/4X  (_santoni_)
+=====================================================
 
 Basic   | Spec Sheet
 -------:|:-------------------------
-CPU     | Quad-core 1.5 GHz ARM® Cortex™ A53 and quad-core 1.2 GHz ARM® Cortex™ A53
-CHIPSET | Qualcomm MSM8939 Snapdragon 615
-GPU     | Adreno 405
-Memory  | 16GB ROM: 3GB RAM.
-Shipped Android Version | 5.1.1
-Battery | 2600 mAh
-Display | 5-inch (1280 x 720 pixels) HD AMOLED display
-Rear Camera  | 13 MP autofocus, flash
-Front Camera | 5.0 MP autofocus flash
+CPU     | Octa-core 1.4 GHz Cortex-A53
+CHIPSET | Qualcomm MSM8937 Snapdragon 435
+GPU     | Adreno 505
+Memory  | 2 GB/3 GB/4 GB
+Shipped Android Version | Android 6.0.1 with MIUI 8
+Storage | 16 GB/32 GB
+MicroSD | Up to 256 GB (Hybrid)
+Battery | 41000 mAh (non-removable)
+Dimensions | 139.2 x 70 x 8.7 mm
+Display | 720 x 1280 pixels, 5.0" IPS
+Rear Camera  | 13.0 MP, LED flash
+Front Camera | 5.0 MP
+Release Date | May 2017
 
-![LYF Water 8](https://assets.gadgets360cdn.com/shop/assets/products/lyf-water-8_1480331300.jpeg "LYF Water 8")
+![Xiaomi Redmi 4[IN]/4X](https://cdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-4x-2.jpg "Xiaomi Redmi 4[IN]/4X")
